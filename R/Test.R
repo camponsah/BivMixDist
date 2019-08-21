ab7acf50c84dc42db017b2c9820656e3f5be7644
camponsah <charles.kamponsah@gmail.com>
2019-08-21 14:25:02 -0700

    Test of exponential versus Pareto type II

---
+++ R/Test.R
@@ -0,0 +1,43 @@
+#' Test of exponential versus Pareto type II (Lomax) distributions
+#'
+#' Performs \eqn{H_0:\alpha =0 \quad vs \quad H_0:\alpha =0}.  \eqn{\alpha =0} corresponds to exponetial distribution and \eqn{\alpha >0} corresponds to Pareto type II distribution.
+#'
+#' expPareto_test is the function for testing exponential vs. Pareto type II distrbutions
+#'
+#' @param X data-vector.
+#' @param alpha paramter which must be numeric greater than  0.
+#' @param beta  paramter which must be numeric greater than  0.
+#' @param p numeric parameter between 0 and 1.
+#'
+#' @return  vector of samples generate from BLG distribution.
+#'
+#' @examples
+#' X<- Renext:: rlomax(100,scale = 0.200,shape = 0.5)
+#' #X<-rexp(300, rate = 1/10)
+#' test<- expPareto_test
+#' test
+#'
+#'@references  Arendarczyk, M. and Kozubowski T. J. and Panorska, A. k. (2018). A bivariate distribution with Lomax and geometric margins . Journal of the Korean Statistical Society, 47:405-422.
+#' \url{https://doi.org/10.1016/j.jkss.2018.04.006}
+#'
+#' @export
+expPareto_test <- function(X)
+{
+  test<- "Exponential vs. Pareto Type II test"
+  data_name <- deparse(substitute(X))
+  X <- X[!is.na(X)]
+  n <- length(X)
+  if (is.numeric(X))
+    X <- as.double(X)
+  else stop("argument 'X' must be numeric")
+  ## statistic
+  statistic <- sum(X^2)/(sum(X))^2
+  #p-value calculation
+  z <-  sqrt(n)*( ((n*statistic)/2) -1 )
+  p_value<- pnorm(z,lower.tail = FALSE)
+  ###Output
+  result<-list(statistic=statistic, p.value=p_value, alternative="greater", data.name=data_name,
+               method=test)
+  class(result)<- "htest"
+  return(result)
+}
