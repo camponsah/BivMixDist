8e12a1813fde60307b39d0439d198838067fa93b
camponsah <charles.kamponsah@gmail.com>
2019-08-22 11:41:25 -0700

    BMEG distribution/EMalgorithm

--- R/mixbeg_EM.R
+++ R/mixbeg_EM.R
@@ -1,78 +1,3 @@
-#' EM algorithm function for the mixed bivariate distribution with mixture of exponential and geometric marginals (BMEG)
-#'
-#' This function computes the parameter estimates of BMEG distribution using the EM algorithm.
-#'
-#' Takes initial guess for the parameters and the algorithm will estmate the MLE.
-#'
-#' @param data  dataframe of bivariate random vector (X,N) BMEG distribution
-#' @param beta scaled paramters vectors for the mixture components, which must be numeric greater than 0
-#' @param q is vector of probabilities for belong to the components, which must between 0 and 1 and the sum equal to 1
-#' @param K  number of mixture components
-#' @param maxiter maximum number of iterations
-#' @param tol tolerance value
-#'
-#' @return  list containg parameter estimates, Deviance and data frame of iteration
-#'
-#' @examples
-#' data.df<-rbmixexpgeo(20, beta=c(1,2),p=0.6,q=c(0.4,0.6))
-#' fit<-bmixexpgeo_em(data.df,K=2)
-#' fit
-#'
-#'@references  Amponsah, C. K.,  Kozubowski, T. J. and Panorska (2019). A computational approach to estimation of discrete Pareto parameters. Inprint.
-#'
-#' @export
-bmixexpgeo_em <- function(data, beta = NULL,q=NULL, K=2, maxiter = 500, tol = 1e-16){
-  N <- data[,2]
-  X<- data[,1]
-  n <- length(N)
-  # log-likelihood for BMEG distribution
-  log_like<- function(data,beta,p,q){
-    ## funct computes log of the sums of pdfs
-    funct<-function(dat.df){
-     pd<- q*(beta^dat.df[2])*(dat.df[1]^(dat.df[2]-1)) *
-       exp(-beta*dat.df[1])*p*(1-p)^(dat.df[2]-1) /gamma(dat.df[2])
-     return(log(sum(pd)))
-    }
-   ll<-apply(data, 1, funct)
-    return(sum(ll))
-  }
-  #initialize beta and q
-  if (is.null(beta) && is.null(q)){
-    U<- stats:: runif(K)
-    q<- U/sum(U)
-  }
-  #extimation of p
-  p<-1/mean(N)
-  # log-liklihood calcultion
-  Devianceold<- 0
-  Deviancenew <- log_like(data,beta,p,q)
-  # Intitialize vectors for storing data
-   k = 1
-  while((abs(Deviancenew - Devianceold) > tol) & (k <= maxiter)){
-    ### E step
-    ## funct computes log of the sums of pdfs
-    tau_funct<-function(dat.df){
-      pd<- q*(beta^dat.df[2])*(dat.df[1]^(dat.df[2]-1)) *
-        exp(-beta*dat.df[1])*p*(1-p)^(dat.df[2]-1) /gamma(dat.df[2])
-      return(pd/sum(pd))
-    }
-    tau<-apply(data, 1, tau_funct)
-    #### M step
-    q<-apply(tau, 1, mean)
-
-    beta<- 0##############
-    p<- p
-
-    Devianceold<-Deviancenew
-    Deviancenew <- log_like(data,beta,p,q)
-    # Output
-    k<- k + 1
-  }
-  result <- list(beta=beta, q=q, p=p, log.like=Deviancenew, Iterations=k)
-  return(result)
-}
-
-
 #' The mixed bivariate distribution with mixture of exponential and geometric marginals (BMEG)
 #'
 #' Random sample generating function for the mixed bivariate distribution with mixture of exponential and geometric marginals (BMEG).
@@ -196,6 +121,83 @@ pbmixexpgeo<- function(data,beta,p,q, lower.tail=TRUE,log.p=FALSE){
     return(1-M)
   }
 }
+
+
+#' EM algorithm function for the mixed bivariate distribution with mixture of exponential and geometric marginals (BMEG)
+#'
+#' This function computes the parameter estimates of BMEG distribution using the EM algorithm.
+#'
+#' Takes initial guess for the parameters and the algorithm will estmate the MLE.
+#'
+#' @param data  dataframe of bivariate random vector (X,N) BMEG distribution
+#' @param beta scaled paramters vectors for the mixture components, which must be numeric greater than 0
+#' @param q is vector of probabilities for belong to the components, which must between 0 and 1 and the sum equal to 1
+#' @param K  number of mixture components
+#' @param maxiter maximum number of iterations
+#' @param tol tolerance value
+#'
+#' @return  list containg parameter estimates, Deviance and data frame of iteration
+#'
+#' @examples
+#' data.df<-rbmixexpgeo(20, beta=c(1,2),p=0.6,q=c(0.4,0.6))
+#' fit<-bmixexpgeo_em(data.df,K=2)
+#' fit
+#'
+#'@references  Amponsah, C. K.,  Kozubowski, T. J. and Panorska (2019). A computational approach to estimation of discrete Pareto parameters. Inprint.
+#'
+#' @export
+bmixexpgeo_em <- function(data, beta =NULL,q=NULL, K=2, maxiter = 500, tol = 1e-16){
+  N <- data[,2]
+  X<- data[,1]
+  n <- length(N)
+  # log-likelihood for BMEG distribution
+  log_like<- function(data,beta,p,q){
+    ## funct computes log of the sums of pdfs
+    funct<-function(dat.df){
+      pd<- q*(beta^dat.df[2])*(dat.df[1]^(dat.df[2]-1)) *
+        exp(-beta*dat.df[1])*p*(1-p)^(dat.df[2]-1) /gamma(dat.df[2])
+      return(log(sum(pd)))
+    }
+    ll<-apply(data, 1, funct)
+    return(sum(ll))
+  }
+  #initialize beta and q
+  if (is.null(beta) && is.null(q)){
+    U<- stats:: runif(K)
+    q<- U/sum(U)
+    beta<- 1
+  }
+  #extimation of p
+  p<-1/mean(N)
+  # log-liklihood calcultion
+  Devianceold<- 0
+  Deviancenew <- log_like(data,beta,p,q)
+  # Intitialize vectors for storing data
+  k = 1
+  while((abs(Deviancenew - Devianceold) > tol) & (k <= maxiter)){
+    ### E step
+    ## funct computes log of the sums of pdfs
+    tau_funct<-function(dat.df){
+      pd<- q*(beta^dat.df[2])*(dat.df[1]^(dat.df[2]-1)) *
+        exp(-beta*dat.df[1])*p*(1-p)^(dat.df[2]-1) /gamma(dat.df[2])
+      return(pd/sum(pd))
+    }
+    tau<-apply(data, 1, tau_funct)
+    #### M step
+    q<-apply(tau, 1, mean)
+
+    beta<- 1##############
+    p<- p
+
+    Devianceold<-Deviancenew
+    Deviancenew <- log_like(data,beta,p,q)
+    # Output
+    k<- k + 1
+  }
+  result <- list(beta=beta, q=q, p=p, log.like=Deviancenew, Iterations=k)
+  return(result)
+}
+
 
 
 
