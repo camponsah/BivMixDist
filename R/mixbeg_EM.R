34b82425ac326aeaec75be2331a67397acf50ea0
camponsah <charles.kamponsah@gmail.com>
2019-08-28 16:20:14 -0700

    BMEG distribution/EMalgorithm

--- R/mixbeg_EM.R
+++ R/mixbeg_EM.R
@@ -129,78 +129,69 @@ pbmixexpgeo<- function(data,beta,p,q, lower.tail=TRUE,log.p=FALSE){
 #'
 #' Takes initial guess for the parameters and the algorithm will estmate the MLE.
 #'
-#' @param data  dataframe of bivariate random vector (X,N) BMEG distribution
-#' @param beta scaled paramters vectors for the mixture components, which must be numeric greater than 0
-#' @param q is vector of probabilities for belong to the components, which must between 0 and 1 and the sum equal to 1
-#' @param K  number of mixture components
-#' @param maxiter maximum number of iterations
-#' @param tol tolerance value
+#' @param data  dataframe of bivariate random vector (X,N) BMEG distribution.
+#' @param beta scaled paramters vectors for the mixture components, which must be numeric greater than 0.
+#' @param q is vector of probabilities for belong to the components, which must between 0 and 1 and the sum equal to 1.
+#' @param k  number of mixture components
+#' @param maxiter maximum number of iterations.
+#' @param tol tolerance value.
+#' @param verb If TRUE, estimates are printed during each iteration.
 #'
 #' @return  list containg parameter estimates, Deviance and data frame of iteration
 #'
 #' @examples
 #' data.df<-rbmixexpgeo(20, beta=c(1,2),p=0.6,q=c(0.4,0.6))
-#' fit<-bmixexpgeo_em(data.df,K=2)
-#' fit
+#' fit<-bmixexpgeo_em(data.df,k=2)
+#' fit$par
 #'
 #'@references  Amponsah, C. K.,  Kozubowski, T. J. and Panorska (2019). A computational approach to estimation of discrete Pareto parameters. Inprint.
 #'
 #' @export
-bmixexpgeo_em <- function(data, beta =NULL,q=NULL, k=2, maxiter = 500, tol = 1e-16){
-
+bmixexpgeo_em <- function(data, beta =NULL,q=NULL, k=2, maxiter = 1000,
+                          tol = 1e-08,verb=FALSE){
   N <- data[,2]
   X<- data[,1]
   n <- length(N)
-
-  # log-likelihood for BMEG distribution
-  log_like<- function(data,beta,p,q){
-    ## funct computes log of the sums of pdfs
-    funct<-function(dat.df){
-      pd<- q*(beta^dat.df[2])*(dat.df[1]^(dat.df[2]-1)) *
-        exp(-beta*dat.df[1])*p*(1-p)^(dat.df[2]-1) /gamma(dat.df[2])
-      return(log(sum(pd)))
-    }
-    ll<-apply(data, 1, funct)
-    return(sum(ll))
-  }
-
-  #initialize beta and q
-  if (is.null(beta) | is.null(q)){
-    fit <- bmixexpgeo.init(data,beta = beta, q=q, k=k)
-    beta <- fit$beta
-    q <- fit$q
-  }
-
-  #extimation of p
+  fit <- bmixexpgeo.init(X=data,beta = beta, q=q, k=k)
+  beta <- fit$beta
+  q <- fit$q
   p<-1/mean(N)
-  # log-liklihood calcultion
-  Devianceold<- 0
-  Deviancenew <- log_like(data,beta,p,q)
-  # Intitialize vectors for storing data
-  j <- 0
-
-  while((abs(Deviancenew - Devianceold) > tol) && (j <= maxiter)){
-    ### E step
-    tau_funct<-function(dat.df){
-      pd<- q*(beta^dat.df[2])*exp(-beta*dat.df[1])
-      return(pd/sum(pd))
+  dens<- function(beta,p,q){
+    pd<-NULL
+    for(i in 1:k){
+     pd<- cbind(pd, stats::dgamma(X,shape = N,rate = beta[i])* q[i]* p* (1-p)^(N-1))
     }
-
-    tau<-apply(data, 1, tau_funct)
-    #### M step
-    q<-apply(tau, 1, mean)
-    b1<-t(tau)*data[,2]
-    b2<- t(tau)*data[,1]
-    beta<- apply(b1 , 2, mean)/apply(b2 , 2, mean)
-    p<- p
-    Devianceold<-Deviancenew
-    Deviancenew <- log_like(data,beta,p,q)
-    # Output
-    j<- j + 1
+    return(pd)
+    }
+  ll.old <- sum(log(apply(dens(beta = beta,p=p,q=q),1,sum)))
+  diff <- 1 + tol
+  it <- 0
+  while(diff > tol && it < maxiter){
+    z <- dens(beta = beta,p=p,q=q)
+    tau<- z/apply(z, 1, sum)
+    q<-apply(tau, 2, mean)
+    beta<- apply(N *tau, 2, sum)/apply(X *tau, 2, sum)
+    ll.new <- sum(log(apply(dens(beta = beta,p=p,q=q),1,sum)))
+    diff <- ll.new - ll.old
+    ll.old <- ll.new
+    it <- it +1
+    if (verb) {
+      cat("iteration =", it, " log.lik diff =", diff, " log.lik =",
+          ll.new, "\n")
+    }
   }
-  result <- list(beta=beta, q=q, p=p, log.like=Deviancenew, Iterations=j)
+  if (it == maxiter) {
+    cat("Warning! Convergence not achieved!", "\n")
+  }
+  beta <- rbind(beta)
+  colnames(beta)=c(paste("comp", ".", 1:k, sep = ""))
+  q <- rbind(q)
+  colnames(q)=c(paste("comp", ".", 1:k, sep = ""))
+  result <- list(data=data,par=list(beta=beta,q=q,p=p), log.like=ll.new,
+                 Iterations=it, posterior=tau, ft="bmixexpgeo_em")
+
+  class(result)<- "mixEM"
   return(result)
-  class(result)<- "htest"
 }
 
 
@@ -211,38 +202,39 @@ bmixexpgeo_em <- function(data, beta =NULL,q=NULL, k=2, maxiter = 500, tol = 1e-
 #'
 #' Takes initial guess for the parameters and the algorithm will estmate the MLE.
 #'
-#' @param x  data-vector X from BMEG distribution
+#' @param X  data-vector X from BMEG distribution
 #' @param beta scale paramters vectors for the mixture components, which must be numeric greater than 0
 #' @param q is vector of probabilities for belong to the components, which must between 0 and 1 and the sum equal to 1
-#' @param K  number of mixture components
+#' @param k  number of mixture components
 #'
 #' @return  list containg intial parameter estimates of beta and q
-#'@references  Code adapted from Derek Young [aut, cre], Tatiana Benaglia [aut], Didier Chauveau [aut], David Hunter [aut], Ryan Elmore [ctb], Thomas Hettmansperger [ctb], Hoben Thomas [ctb], Fengjuan Xuan [ctb]. mixtools: Tools for Analyzing Finite Mixture Models, R CRAN.
+#'@references  Code adapted from Young et al (2017). mixtools Package : Tools for Analyzing Finite Mixture Models, R CRAN.
 #'
+#' @export
 bmixexpgeo.init <- function(X, beta = NULL, q = NULL, k=2){
   x <- X[,1]
   N <- X[,2]
   n <- length(x)
 
   if (is.null(q)) {
-    U = runif(k)
-    q = U/sum(U)
-  } else k = length(q)
+    U <- stats:: runif(k)
+    q <- U/sum(U)
+  } else k <- length(q)
 
   if(k==1){
-    x.bar=mean(x)
+    x.bar<- mean(x)
   } else{
-    x.sort=sort(x)
+    x.sort<- sort(x)
     ind=floor(n*cumsum(q))
-    x.part=list()
-    x.part[[1]]=x.sort[1:(ind[1]+1)]
+    x.part<-list()
+    x.part[[1]] <- x.sort[1:(ind[1]+1)]
     for(j in 2:k){
-      x.part[[j]]=x.sort[ind[j-1]:ind[j]]
+      x.part[[j]] <- x.sort[ind[j-1]:ind[j]]
     }
-    x.bar=sapply(x.part,mean)
+    x.bar <- sapply(x.part,mean)
   }
   if(is.null(beta)){
-    beta=mean(N)/x.bar
+    beta <- mean(N)/x.bar
   }
 
   list( q=q, beta=beta, k=k)
